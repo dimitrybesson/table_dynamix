a16ea509ace98b163f0f604891777b7ebbf606c2
Dimitry Besson <dimitrybesson@gmail.com>
2016-04-15 17:03:36 -0400

    Add reservation view, controller actions, routes

--- tabledynamix/app/controllers/reservations_controller.rb
+++ tabledynamix/app/controllers/reservations_controller.rb
@@ -1,10 +1,26 @@
 class ReservationsController < ApplicationController
+
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reservation = @restaurant.reservations.build
   end
 
-  def create
+  def create # add party_size validation vs capacity
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reservation = @restaurant.reservations.build(reservation_params)
+    @reservation.customer = current_user
+    if @reservation.save
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   def index
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:date, :time, :party_size)
   end
 end
