b1e86a0b8abc5c15e01c500904b7017eaf8970c6
Dimitry Besson <dimitrybesson@gmail.com>
2016-04-16 16:00:06 -0400

    Added validations for restaurant and for reservation during operating hours

--- tabledynamix/app/models/reservation.rb
+++ tabledynamix/app/models/reservation.rb
@@ -2,6 +2,7 @@ class Reservation < ActiveRecord::Base
   belongs_to :restaurant
   belongs_to :customer
   validate :can_accomodate_party
+  validate :during_operating_hours
 
   def can_accomodate_party
   #binding.pry
@@ -12,6 +13,12 @@ class Reservation < ActiveRecord::Base
 
     if (restaurant.capacity.to_i - taken_spots.to_i) < party_size.to_i
       errors.add(:over_capacity, "Sorry, not enough seats")
+    end
+  end
+
+  def during_operating_hours
+    if (time < restaurant.open_time) || (time >= restaurant.close_time)
+      errors.add(:not_open, "Sorry, we are not open at that time")
     end
   end
 end
--- tabledynamix/app/models/restaurant.rb
+++ tabledynamix/app/models/restaurant.rb
@@ -2,4 +2,12 @@ class Restaurant < ActiveRecord::Base
   belongs_to :owner
   has_many :reservations
   has_many :customers, through: :reservations
+
+  validates :name, :capacity, :phone, :open_time, :close_time, presence: true
+  validates :capacity, numericality: {only_integer: true, greater_than: 0}
+  validates :price, numericality: {only_integer: true, greater_than: 0, less_than: 4}
+  ##phone number
+
+  ##description
+  ##menu
 end
