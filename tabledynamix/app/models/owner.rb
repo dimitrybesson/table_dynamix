6a61e4c880013d14b844a042a4513dafcfb87f51
Amin Shafiee <Amin@Amins-MacBook-Pro.local>
2016-04-17 15:20:58 -0400

    model_validation_almost_complete

--- tabledynamix/app/models/owner.rb
+++ tabledynamix/app/models/owner.rb
@@ -3,4 +3,6 @@ class Owner < ActiveRecord::Base
   has_many :restaurants
   has_many :reservations, through: :restaurants
   has_many :customers, through: :reservations
+  validates :email, :password, :password_confirmation, presence: true
+  validates :email, uniqueness: true
 end
