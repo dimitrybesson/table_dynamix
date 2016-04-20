4c30b0a7cdcdf2d744d4b79571b81892f98e788c
Dimitry Besson <dimitrybesson@gmail.com>
2016-04-20 10:08:19 -0400

    Merged rake task email reminders

--- tabledynamix/config/environments/development.rb
+++ tabledynamix/config/environments/development.rb
@@ -17,15 +17,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   # Uncomment to go weapons hot
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'example.com',
-    user_name: 'tabledynamix@gmail.com',
-    password: 'bitmakerlabs',
-    authentication: 'plain',
-    enable_starttls_auto: true}
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   domain: 'example.com',
+  #   user_name: 'tabledynamix@gmail.com',
+  #   password: 'bitmakerlabs',
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
